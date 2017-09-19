d1f51982b0177f8e9d81d561da2bdd3bf32cac3a
Brian Dentino <brian.dentino@gmail.com>
2017-09-18 18:09:21 -0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM alpine:edge
+
+ENV MMS_VERSION 6.0.0.381-1
+
+RUN adduser -S mms
+
+WORKDIR /home/mms
+
+RUN apk --update add curl libsasl ca-certificates su-exec && \
+    curl -sL https://cloud.mongodb.com/download/agent/monitoring/mongodb-mms-monitoring-agent-${MMS_VERSION}.linux_x86_64.tar.gz | tar -xz --strip-components=1 && \
+    chmod +w monitoring-agent.config && \
+    apk del --purge curl && \
+    rm -rf /var/cache/apk/* && \
+    mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2 && ln -s /usr/lib/libsasl2.so.3 /usr/lib/libsasl2.so.2
+
+COPY entrypoint.sh .
+
+ENTRYPOINT [ "/home/mms/entrypoint.sh" ]
+
+CMD [ "/home/mms/mongodb-mms-monitoring-agent", "-loglevel", "warn" ]
---
+++ entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+cd "${0%/*}"
+set -e
+
+: ${MMS_SERVER:=https://mms.mongodb.com}
+: ${MMS_MUNIN:=true}
+: ${MMS_CHECK_SSL_CERTS:=true}
+
+if [ ! "$MMS_API_KEY" ] || [ ! "$MMS_GROUP_ID" ]; then
+  {
+    echo 'error: MMS_API_KEY or MMS_GROUP_ID was not specified'
+    echo 'try something like: docker run -e MMS_API_KEY=... MMS_GROUP_ID=...'
+    echo '(see https://mms.mongodb.com/settings/monitoring-agent for your mmsApiKey)'
+    echo
+    echo 'Other optional variables:'
+    echo ' - MMS_SERVER='"$MMS_SERVER"
+    echo ' - MMS_MUNIN='"$MMS_MUNIN"
+    echo ' - MMS_CHECK_SSL_CERTS='"$MMS_CHECK_SSL_CERTS"
+  } >&2
+  exit 1
+fi
+
+set_config() {
+  key="$1"
+  value="$2"
+  sed_escaped_value="$(echo "$value" | sed 's/[\/&]/\\&/g')"
+  sed -ri "s/^($key)[ ]*=.*$/\1 = $sed_escaped_value/" monitoring-agent.config
+}
+
+set_config mmsApiKey "$MMS_API_KEY"
+set_config mmsBaseUrl "$MMS_SERVER"
+set_config enableMunin "$MMS_MUNIN"
+set_config mmsGroupId "$MMS_GROUP_ID"
+set_config sslRequireValidServerCertificates "$MMS_CHECK_SSL_CERTS"
+
+chown mms monitoring-agent.config
+chown mms mongodb-mms-monitoring-agent
+
+su-exec mms "$@"
